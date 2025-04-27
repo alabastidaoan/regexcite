cb87141cb5f2726a4bee334fc6dbcf9e49c9c3cc
a.labastida.oan <a.labastida.oan@gmail.com>
2025-04-27 00:52:55 -0600

    Package main contents

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Split a long string into a character vector
+#'
+#' @param x long string
+#' @param split splitting character
+#'
+#' @returns A character vector
+#' @export
+#'
+#' @examples
+#' long_string<-("alpha,bravo,charlie,delta")
+#' strsplit1(long_string, ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
